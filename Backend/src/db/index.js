f353ac5eb5a4ae68f606e62d959fa77bf797d84b
VaibhavTiwari14 <vaibhavtiwar283@gmail.com>
2025-01-30 17:43:16 +0530

    add database connection setup and constants

---
+++ Backend/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME }from "../../constants.js" ;
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Database connected successfully!! DB HOST: ", connectionInstance.connection.host);
+    } catch (error) {
+        console.log("Error while connecting to database", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
