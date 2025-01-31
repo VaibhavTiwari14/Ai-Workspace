9a5fc69b180ddbb2d8706ea806eb0b1ed2220210
VaibhavTiwari14 <vaibhavtiwar283@gmail.com>
2025-01-31 10:53:11 +0530

    set up Express server with middleware and routes structure

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,20 @@ import  dotenv  from "dotenv";
 
 
 import connectDB from "./src/db/index.js";
+import {app} from "./app.js";
 
 
 dotenv.config({
         path:'./.env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+        app.listen(process.env.PORT, () => {
+                console.log(`Server is running on port ${process.env.PORT}`);
+        }
+)})
+.catch((error) => {
+        console.log("Error while connecting to database", error);
+        process.exit(1);
+});
